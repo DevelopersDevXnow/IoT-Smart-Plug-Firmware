574b7a7a60a472b1b030e2fe62f3752d9c8b3762
DevelopersDevXnow <developers.devxnow@gmail.com>
2022-11-19 13:02:49 +0530

    initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,20 @@
+
+/*
+ * Author: Royston Sanctis
+ * Date: 13 November 2022
+ * Board ESP32 DEVKIT V1
+ * Project: Smart Plug
+ * Email: roystonsanctis@gmail.com
+ */
+
+#include <Arduino.h>
+
+void setup()
+{
+  // put your setup code here, to run once:
+}
+
+void loop()
+{
+  // put your main code here, to run repeatedly:
+}
